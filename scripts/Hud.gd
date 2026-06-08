db6d783eace1a4dafafb57c3ef9be12716646c12
Larry <garry.system.dev@gmail.com>
2026-06-07 19:36:33 -0500

    Game feel: player HUD health bar + shift wave label for pause button

--- scripts/Hud.gd
+++ scripts/Hud.gd
@@ -8,6 +8,8 @@ var _wave_label: Label
 var _boss_bar: ProgressBar
 var _ammo_label: Label
 var _reload_bar: ProgressBar
+var _hp_bar: ProgressBar
+var _hp_label: Label
 var _player: Player
 
 func _ready() -> void:
@@ -27,11 +29,26 @@ func _ready() -> void:
 	_label.offset_top = 32
 	add_child(_label)
 
+	_hp_bar = ProgressBar.new()
+	_hp_bar.show_percentage = false
+	_hp_bar.max_value = 1.0
+	_hp_bar.offset_left = 14
+	_hp_bar.offset_right = 214
+	_hp_bar.offset_top = 54
+	_hp_bar.offset_bottom = 72
+	_hp_bar.modulate = Color(1.0, 0.45, 0.45)   # red-ish, distinct from the XP bar
+	add_child(_hp_bar)
+
+	_hp_label = Label.new()
+	_hp_label.offset_left = 18
+	_hp_label.offset_top = 53
+	add_child(_hp_label)
+
 	_wave_label = Label.new()
 	_wave_label.anchor_left = 1.0
 	_wave_label.anchor_right = 1.0
 	_wave_label.offset_left = -180
-	_wave_label.offset_right = -12
+	_wave_label.offset_right = -76
 	_wave_label.offset_top = 36
 	_wave_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 	add_child(_wave_label)
@@ -76,6 +93,8 @@ func _process(_delta: float) -> void:
 	_bar.max_value = _player.xp_to_next()
 	_bar.value = _player.xp
 	_label.text = "Level %d" % _player.level
+	_hp_bar.value = _player.health_fraction()
+	_hp_label.text = "HP %d / %d" % [int(_player.hp()), int(_player.max_hp())]
 	if RunConfig.mode == "boss_rush":
 		var spawner := get_tree().get_first_node_in_group("spawner")
 		var n := 0
